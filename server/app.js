c1773f94c79ac9f78e590fc6f2dd241ebbf98cf5
Sean T. Purvis <sean.purvis@usm.edu>
2019-02-03 23:38:55 -0600

    Add CORS Support

--- server/app.js
+++ server/app.js
@@ -9,6 +9,17 @@ const imageRoutes = require('./api/routes/images');
 // Request logger
 app.use(morgan('dev'));
 
+// Handle CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        return res.status(200).json({});
+    }
+});
+
 // Routes that should handle requests
 app.use('/api/v1/images', imageRoutes);
 
