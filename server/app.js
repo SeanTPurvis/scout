a0aabc065bfb6188e8bb657631a5a18f5ac328b1
Sean T. Purvis <sean.purvis@usm.edu>
2019-02-03 23:30:37 -0600

    Add Basic Router for Images

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+
+// Include custom Routers
+const imageRoutes = require('./api/routes/images');
+
+// Request logger
+app.use(morgan('dev'));
+
+// Routes that should handle requests
+app.use('/api/v1/images', imageRoutes);
+
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: "Hello, World!"
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
