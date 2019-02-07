a99893fa0cf49742d4c1cf6f642c99106768f61c
Sean T. Purvis <sean.purvis@usm.edu>
2019-02-06 19:11:07 -0600

    Add File API
    Had to disable CORS support temporarily. Will re implement soon.

--- server/app.js
+++ server/app.js
@@ -1,46 +1,118 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://SeanTPurvis:" + process.env.MONGO_ATLAS_PASSWORD + "@scout-shard-00-00-iszaz.mongodb.net:27017,scout-shard-00-01-iszaz.mongodb.net:27017,scout-shard-00-02-iszaz.mongodb.net:27017/test?ssl=true&replicaSet=Scout-shard-0&authSource=admin&retryWrites=true",
-{
-    useMongoClient: true
-})
+// Mongo URI
+const mongoURI = 'mongodb+srv://sean:sean@scout-iszaz.mongodb.net/test?retryWrites=true';
 
-// Include custom Routers
-const imageRoutes = require('./api/routes/images');
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
 
-// Request logger
+// Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-// Handle CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+// Init gfs
+let gfs;
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+});
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-        return res.status(200).json({});
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
     }
+  });
+  const upload = multer({ storage });
+
+// POST /upload
+// Uploads files to database
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.json({file: req.file});
 });
 
-// Routes that should handle requests
-app.use('/api/v1/images', imageRoutes);
+// GET /files
+// Display all files in JSON
+app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        // Check for files
+        if (!files || files.length === 0) {
+            return res.status(404).json({
+                err: 'No files exist'
+            });
+        }
 
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+        return res.json(files);
+    });
+}); 
+
+// GET /files/:filename
+// Display single object in JSON
+app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        };
+
+        return res.json(file);
+    });
 });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
+// GET /image/:filename
+// Display image
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        };
+
+        // Check if image
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+            // Read stream to browser
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({
+                err: 'Not an image'
+            })
         }
     });
 });
+
+// DELETE /files/:id
+// Delete file
+app.delete('/files/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+      if (err) {
+        return res.status(404).json({ err: err });
+      }
+    });
+  });
 
 module.exports = app;
