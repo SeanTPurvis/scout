60df1f001814dc5bb21bb248d3334df8ec4aa201
Sean T. Purvis <sean.purvis@usm.edu>
2019-02-17 20:15:32 -0600

    Protect Admin Routes

--- server/routes/accelerometers.js
+++ server/routes/accelerometers.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const Auth = require('../middleware/auth');
+const AdminAuth = require('../middleware/admin-auth');
 
 const AccelerometerController = require('../controllers/accelerometer');
 
-router.post('/', AccelerometerController.accelerometer_create);
+router.post('/', Auth, AccelerometerController.accelerometer_create);
 
-router.get('/', AccelerometerController.accelerometer_get_all);
+router.get('/', Auth, AccelerometerController.accelerometer_get_all);
 
-router.get('/:accId', AccelerometerController.accelerometer_get_one);
+router.get('/:accId', Auth, AccelerometerController.accelerometer_get_one);
 
-router.delete('/:accId', AccelerometerController.accelerometer_delete_one);
+router.delete('/:accId', AdminAuth, AccelerometerController.accelerometer_delete_one);
 
-router.delete('/security/:userEmail', AccelerometerController.accelerometer_delete_many_email);
+router.delete('/security/:userEmail', AdminAuth, AccelerometerController.accelerometer_delete_many_email);
 
 module.exports = router;
--- server/routes/gyroscopes.js
+++ server/routes/gyroscopes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const Auth = require('../middleware/auth');
+const AdminAuth = require('../middleware/admin-auth');
 const GyroscopeController = require('../controllers/gyroscope');
 
-router.post('/', GyroscopeController.gyroscope_create);
+router.post('/', Auth, GyroscopeController.gyroscope_create);
 
-router.get('/', GyroscopeController.gyroscope_get_all);
+router.get('/', Auth, GyroscopeController.gyroscope_get_all);
 
-router.get('/:gyroId', GyroscopeController.gyroscope_get_one);
+router.get('/:gyroId', Auth, GyroscopeController.gyroscope_get_one);
 
-router.delete('/:gyroId', GyroscopeController.gyroscope_delete_one);
+router.delete('/:gyroId', AdminAuth, GyroscopeController.gyroscope_delete_one);
 
-router.delete('/security/:userEmail', GyroscopeController.gyroscope_delete_many_email);
+router.delete('/security/:userEmail', AdminAuth, GyroscopeController.gyroscope_delete_many_email);
 
 module.exports = router;
