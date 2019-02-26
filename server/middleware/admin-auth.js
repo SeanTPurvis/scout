1a437f73242b85cf7a269ae1714cdececbdecec9
Sean T. Purvis <sean.purvis@usm.edu>
2019-02-25 22:51:54 -0600

    Modify User Delete
    Disallow the user to delete themselves

--- server/middleware/admin-auth.js
+++ server/middleware/admin-auth.js
@@ -2,18 +2,12 @@ const jwt = require('jsonwebtoken');
 
 // if Authentication is successful, call next, if not, fail
 module.exports = (req, res, next) => {
-    if (process.env.AUTH_MODE == "dev") {
-        next();
-    }
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY, null);
-        req.userData = decoded;
-        if (decoded.admin == true) {
-            next();
-        } else {
-            throw('Auth failed');
-        }
+        res.locals.userData = decoded;
+        if (decoded.admin != true) throw new Error("Admin auth failed")
+        next();
     } catch(error) {
         return res.status(401).json({
             message: 'Auth failed'
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,13 +2,10 @@ const jwt = require('jsonwebtoken');
 
 // if Authentication is successful, call next, if not, fail
 module.exports = (req, res, next) => {
-    if (process.env.AUTH_MODE == "dev") {
-        next();
-    }
     try {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY, null);
-        req.userData = decoded;
+        res.locals.userData = decoded;
         next();
     } catch(error) {
         return res.status(401).json({
